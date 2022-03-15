8f8413337d755be246f51bb26d52a6118ebfb15f
andrewquang512 <andrew.viquang@gmail.com>
2022-03-15 17:14:34 +0700

    fix for deploying

--- webclient/src/URLs.js
+++ webclient/src/URLs.js
@@ -1,14 +1,14 @@
 let URLs = {};
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   URLs = {
-    baseURL: "/api",
-    socketURL: "https://thoughtwall.herokuapp.com/api",
+    baseURL: '/api',
+    socketURL: 'https://bku-ces-iotsmarthouse.herokuapp.com/api',
   };
 } else {
   URLs = {
-    baseURL: "http://localhost:5000/api",
-    socketURL: "http://localhost:5000/api",
+    baseURL: 'http://localhost:5000/api',
+    socketURL: 'http://localhost:5000/api',
   };
 }
 
