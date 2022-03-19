add82435f8a62624d285e4245fa08e5ceca59e1c
unknown <tdhtp2016@gmail.com>
2022-03-19 22:54:50 +0700

    finish Rooms

--- Mobile Application/smart-home/components/Layout/Rooms/RoomsHS.js
+++ Mobile Application/smart-home/components/Layout/Rooms/RoomsHS.js
@@ -7,45 +7,54 @@ const rooms = [
     id: "0",
     name: "LIVINGROOM",
     image: "https://i.ibb.co/YZ0pKzj/livingroom.jpg",
+    route: "Livingroom",
   },
   {
     id: "1",
     name: "BEDROOM",
     image: "https://i.ibb.co/wrHDt4W/bedroom.jpg",
+    route: "Bedroom",
   },
   {
     id: "2",
     name: "KITCHEN",
     image: "https://i.ibb.co/hfKKFqZ/kitchen.jpg",
+    route: "Kitchen",
   },
   {
     id: "3",
     name: "BATHROOM",
     image: "https://i.ibb.co/Df9cpLf/bathroom.jpg",
+    route: "Bathroom",
   },
 ];
 
 const Rooms = (props) => {
   return (
     <Box>
-      <Heading size="xl" px="5" py="3" color="pink.400">
+      <Heading size="xl" px="5" pb="3" color="pink.400">
         Rooms
       </Heading>
+
       <ScrollView
         horizontal={true}
         px="3"
         showsHorizontalScrollIndicator={false}
       >
-        <HStack space={3} justifyContent="center">
+        <HStack space={3} justifyContent="center" mx="1">
           {rooms.map((itemData, index) => (
             <RoomCard
               name={itemData.name}
               image={itemData.image}
+              route={itemData.route}
+              navigation={props.navigation}
               key={index}
             ></RoomCard>
           ))}
         </HStack>
       </ScrollView>
+
+      {/* <RoomCard></RoomCard> */}
     </Box>
   );
 };
---
+++ Mobile Application/smart-home/screens/rooms/KitchenScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const LivingroomScreen = (props) => {
+  return <Button title="asdf"></Button>;
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+  },
+});
+
+export default LivingroomScreen;
