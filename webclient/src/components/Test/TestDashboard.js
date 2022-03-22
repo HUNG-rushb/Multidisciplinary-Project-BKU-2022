eac72d508796af9925f975341ad877397a87e173
andrewquang512 <andrew.viquang@gmail.com>
2022-03-22 21:18:04 +0700

    heroku logs --tail

--- webclient/src/components/Test/TestDashboard.js
+++ webclient/src/components/Test/TestDashboard.js
@@ -4,49 +4,9 @@ import io from 'socket.io-client';
 import axios from 'axios';
 
 const TestDashboard = () => {
-  const [test, setTest] = useState({
-    key: '',
-    name: '',
-    description: '',
-    data: [],
-  });
-
   const currentRef = useRef([]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const connectSocket = async () => {
-    const socket = io(`${URLs.socketURL}/socket`);
-
-    socket.on('newTest', (device) => {
-      setTest({
-        data: device.data,
-        key: device.key,
-        name: device.name,
-        description: device.description,
-      });
-      console.log(test);
-    });
-    socket.on('updateTest', (updateData) => {
-      console.log(updateData);
-      setTest({
-        data: updateData,
-        ...test,
-      });
-      console.log(test);
-    });
-
-    //     socket.on('deletedTest', (id) => {
-    //       const updatedTest = test.data.filter((data) => {
-    //         return data._id !== id;
-    //       });
-    //
-    //       setTest({ data: updatedTest });
-    //       console.log(test);
-    //     });
-
-    // socket.on('thoughtsCleared', () => {
-    //   setTest({ data: [] });
-    // });
-
     await fetchThoughts();
   };
 
@@ -54,19 +14,7 @@ const TestDashboard = () => {
     try {
       const response = await axios.get(`${URLs.baseURL}/devices`);
       if (response.data) {
-        // get first device
         currentRef.current = response.data;
-        console.log(currentRef.current);
-        console.log(currentRef);
-        // if (JSON.stringify(device) !== JSON.stringify(test)) {
-        //   setTest({
-        //     key: device.key,
-        //     name: device.name,
-        //     description: device.description,
-        //     data: device.data,
-        //   });
-        //   console.log(test);
-        // }
       } else {
         alert(response.data);
       }
@@ -81,13 +29,7 @@ const TestDashboard = () => {
   useEffect(() => {
     connectSocket();
   }, [connectSocket]);
-  return (
-    <div>
-      {/* {test.data.map((each, key) => {
-        return <p key={key}>{each.value}</p>;
-      })} */}
-    </div>
-  );
+  return <div>{currentRef}</div>;
 };
 
 export default TestDashboard;
