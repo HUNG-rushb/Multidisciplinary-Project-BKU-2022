3827bb3a2c8f51da1c08339943d76b1a9806058c
tttm1501 <nam0968024068@gmail.com>
2022-04-03 23:25:09 +0700

    Update actions for device, room and type

--- webclient/src/actions/device.js
+++ webclient/src/actions/device.js
@@ -5,6 +5,11 @@ import {
   GET_ERRORS,
   POST_DATA,
   POST_ERROR,
+  ADD_DEVICE,
+  GET_DEVICE_ITEM, 
+  GET_VALUE,
+  UPDATE_DEVICE,
+  DELETE_DEVICE,
 } from '../actions/types';
 import URLs from '../URLs';
 export const loadDevices = () => async (dispatch) => {
@@ -20,6 +25,39 @@ export const loadDevices = () => async (dispatch) => {
     });
   }
 };
+
+export const getDeviceById = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${URLs.baseURL}/device/${id}`);
+
+    dispatch({
+      type: GET_DEVICE_ITEM,
+      payload: res
+    });
+
+    console.log(res);
+  } catch {
+    dispatch({
+      type: GET_ERRORS
+    })
+  }
+}
+
+export const getValueDevice = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${URLs.baseURL}/device/${id}/data`);
+
+    dispatch({
+      type: GET_VALUE,
+      payload: res.data
+    })
+
+  } catch {
+    dispatch({
+      type: GET_ERRORS
+    })
+  }
+}
 
 export const postData = (id, value) => async (dispatch) => {
   const body = {
@@ -62,4 +100,87 @@ export const postData = (id, value) => async (dispatch) => {
   //     type: GET_ERRORS,
   //   });
   // }
+};
+
+export const addNewDevice = (room_name, device_name, description) => async (dispatch) => {
+  const body = {
+    room_name: room_name,
+    device_name: device_name,
+    description: description
+  };
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      `${URLs.baseURL}/devices`,
+      body,
+      config
+    );
+
+    dispatch ({
+      type: ADD_DEVICE,
+      payload: res.data
+    });
+  } catch(error) {
+    dispatch ({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const updateDevice = (id, device_name) => async(dispatch) => {
+  const body = {
+    "device_name": device_name
+  }
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  try {
+    const res = await axios.put(
+      `${URLs.baseURL}/device/${id}`,
+      body,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_DEVICE,
+      payload: res.data
+    });
+  } catch(error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      }
+    })
+  }
+}
+
+export const deleteDevice = (id) => async(dispatch) => {
+  try {
+    const res = await axios.delete(`${URLs.baseURL}/device/${id}`);
+    
+    dispatch({
+      type: DELETE_DEVICE,
+      payload: res.data
+    });
+  } catch(error) {
+    dispatch({
+      type: POST_ERROR
+    });
+  }
 };
--- webclient/src/actions/room.js
+++ webclient/src/actions/room.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_ROOMS, GET_ERRORS } from '../actions/types';
+import { GET_ROOMS, GET_ERRORS, GET_ROOM_ITEM, GET_ROOM_DEVICE, ADD_ROOM, POST_ERROR, UPDATE_ROOM, DELETE_ROOM } from '../actions/types';
 import URLs from '../URLs';
 export const loadRooms = () => async (dispatch) => {
   try {
@@ -13,5 +13,121 @@ export const loadRooms = () => async (dispatch) => {
     dispatch({
       type: GET_ERRORS,
     });
+  }
+};
+
+export const getRoomById = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${URLs.baseURL}/room/${id}`);
+
+    dispatch({
+      type: GET_ROOM_ITEM,
+      payload: res
+    });
+
+    console.log(res);
+  } catch(error) {
+    dispatch({
+      type: GET_ERRORS
+    })
+  }
+};
+
+export const getRoomDevices = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${URLs.baseURL}/room/${id}/devices`);
+
+    dispatch({
+      type: GET_ROOM_DEVICE,
+      payload: res.data
+    });
+
+  } catch(error) {
+    dispatch({
+      type: GET_ERRORS
+    })
+  }
+};
+
+export const addNewRoom = (group_name, description) => async (dispatch) => {
+  const body = {
+    group_name: group_name,
+    description: description
+  };
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      `${URLs.baseURL}/rooms`,
+      body,
+      config
+    );
+
+    dispatch({
+      type: ADD_ROOM,
+      payload: res.data
+    })
+  } catch(error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      }
+    })
+  }
+};
+
+export const updateRoom = (id, group_name, description) => async(dispatch) => {
+  const body = {
+    group_name: group_name,
+    description: description
+  };
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.put(
+      `${URLs.baseURL}/room/${id}`,
+      body,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_ROOM,
+      payload: res.data
+    })
+  } catch(error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      }
+    })
+  }
+};
+
+export const deleteRoom = (id) => async(dispatch) => {
+  try {
+    const res = await axios.delete(`${URLs.baseURL}/room/${id}`);
+
+    dispatch({
+      type: DELETE_ROOM,
+      payload: res.data
+    });
+  } catch(error) {
+    dispatch({
+      type: POST_ERROR
+    })
   }
 };
