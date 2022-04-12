52a5bd72608047230e8f8cca8469870ea39a94ed
Winn <85269588+winnguyen13@users.noreply.github.com>
2022-04-13 00:46:46 +0700

    Update
    update

--- webclient/src/components/Layout/Home.js
+++ webclient/src/components/Layout/Home.js
@@ -1,39 +1,55 @@
 import React, { Fragment } from 'react';
 import profile from '../../images/profile.png';
+import State from './State';
 import DeviceForm from '../Devices/DeviceForm';
 import Warning from './Warning';
 import Dashboard from '../Dashboard/Dashboard';
 
 const Home = () => {
-  return (
-    <Fragment>
-      <Dashboard />
-      <div className='right'>
-        <div className='top'>
-          <button id='menu-btn'>
-            <span className='material-icons-sharp'>menu</span>
-          </button>
-          <div className='wifi'>
-            <span className='material-icons-sharp'>wifi</span>
-          </div>
-          <p>Connected</p>
-          <div className='profile'>
-            <div className='info'>
-              <p>
-                Hey, <b>Name</b>
-              </p>
-              <small className='text-muted'>Admin</small>
-            </div>
-            <div className='profile-photo'>
-              <img src={profile} alt='profile' />
-            </div>
-          </div>
-        </div>
-        <DeviceForm />
-        <Warning />
-      </div>
-    </Fragment>
-  );
+    return ( <
+        Fragment >
+        <
+        Dashboard / >
+        <
+        div className = 'right' >
+        <
+        div className = 'top' >
+        <
+        button id = 'menu-btn' >
+        <
+        span className = 'material-icons-sharp' > menu < /span> < /
+        button > <
+        div className = 'wifi' >
+        <
+        span className = 'material-icons-sharp' > wifi < /span> < /
+        div > <
+        p > Connected < /p> <
+        div className = 'profile' >
+        <
+        div className = 'info' >
+        <
+        p >
+        Hey, < b > Name < /b> < /
+        p > <
+        small className = 'text-muted' > Admin < /small> < /
+        div > <
+        div className = 'profile-photo' >
+        <
+        img src = { profile }
+        alt = 'profile' / >
+        <
+        /div> < /
+        div > <
+        /div>  <
+        State / >
+        <
+        DeviceForm / >
+        <
+        Warning / >
+        <
+        /div> < /
+        Fragment >
+    );
 };
 
 export default Home;
