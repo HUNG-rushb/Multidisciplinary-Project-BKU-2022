bb6c5feb12f892294ddebf95059c2d5f0dd51705
HUNG-rushb <tdhtp2016@gmail.com>
2022-04-21 12:44:04 +0700

    ok

--- Mobile Application/smart-home/components/Layout/Rooms/RoomsHS.js
+++ Mobile Application/smart-home/components/Layout/Rooms/RoomsHS.js
@@ -1,7 +1,17 @@
-import { useState, useEffect } from "react";
-import { Heading, HStack, ScrollView, Box, Spinner } from "native-base";
-import RoomCard from "./RoomCard";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import {
+  Heading,
+  HStack,
+  ScrollView,
+  Box,
+  Spinner,
+  Icon,
+  IconButton,
+} from "native-base";
+import RoomCard from "./RoomCard";
+
+import { MaterialCommunityIcons } from "@native-base/icons";
 
 const Rooms = (props) => {
   const [rooms, setRooms] = useState([]);
@@ -26,18 +36,54 @@ const Rooms = (props) => {
 
   // console.log(rooms);
 
+  const loadData = () => {
+    setRooms([]);
+
+    const getData = async () => {
+      await axios
+        .get("https://bku-ces-iotsmarthouse.herokuapp.com/api/rooms")
+        .then((response) => {
+          // Update rooms state
+          setRooms(
+            response.data.sort((a, b) => (a.room_id < b.room_id ? 1 : -1))
+          );
+        });
+    };
+
+    getData();
+  };
+
   return (
     <Box>
-      <Heading size="xl" px="5" pb="3" color="pink.400">
-        Rooms
-      </Heading>
+      <HStack space={2} justifyContent="space-between">
+        <Heading size="xl" px="5" pb="3" color="pink.400">
+          Rooms
+        </Heading>
+
+        <IconButton
+          icon={
+            <Icon
+              as={MaterialCommunityIcons}
+              name="refresh"
+              size="md"
+              color="pink.300"
+            />
+          }
+          mr={2}
+          mb={2}
+          _pressed={{
+            bg: "pink.100",
+          }}
+          onPress={loadData}
+        />
+      </HStack>
 
       {rooms.length === 0 ? (
         <HStack space={2} justifyContent="center">
           <Spinner size="lg" color="pink.300" />
-          <Heading color="pink.300" fontSize="lg" maxW="50%">
-            Duma đang fetch data chờ tí đi pa...
-          </Heading>
+          {/* <Heading color="pink.300" fontSize="lg" maxW="50%">
+            Loading...
+          </Heading> */}
         </HStack>
       ) : (
         <ScrollView
